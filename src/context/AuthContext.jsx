dfd40f02d96c50e4ff1d94756ec03e380c9ef8de
sametcanerl <sametcaneol28@gmail.com>
2022-07-24 18:03:37 +0300

    cards finished

--- src/context/AuthContext.jsx
+++ src/context/AuthContext.jsx
@@ -3,21 +3,20 @@ import { useState } from "react";
 import { userObserver } from "../auth/firebase";
 import { useEffect } from "react";
 
+export const AuthContext = createContext();
 
-export const AuthContext = createContext()
+const AuthContextProvider = ({ children }) => {
+  const [currentUser, setCurrentUser] = useState(false);
 
-const AuthContextProvider = ({children})=>{
-    const [currentUser, setCurrentUser] = useState(false)
-    useEffect(() => {
-    userObserver(setCurrentUser)
-    }, [])
-    
+  useEffect(() => {
+    userObserver(setCurrentUser);
+  }, []);
 
-    return (
-        <AuthContext.Provider value={{currentUser}}>
-            {children}
-        </AuthContext.Provider>
-    )
-}
+  return (
+    <AuthContext.Provider value={{ currentUser }}>
+      {children}
+    </AuthContext.Provider>
+  );
+};
 
-export default AuthContextProvider
+export default AuthContextProvider;
